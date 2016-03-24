379c6f73582e85cf4b07d6c3ad1b9505f4937b26
Ryan Morey <ryantmorey@gmail.com>
2016-03-24 15:06:30 -0400

    Rework some things

--- rubus.py
+++ rubus.py
@@ -3,10 +3,10 @@ import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
 
 AGENCY = 'rutgers'
-API_URL = "http://webservices.nextbus.com/service/publicXMLFeed?a="+AGENCY
+API_URL = "http://webservices.nextbus.com/service/publicXMLFeed?a="+AGENCY+"&command={c}"
 
 def get_predictions(route,stop):
-    url = API_URL + "&command=predictions&r={}&s={}".format(route,stop)
+    url = API_URL.format(c="predictions") + "&r={}&s={}".format(route,stop)
     root = ET.fromstring(requests.get(url).text)
     for p in root[0][0]:
         t = datetime.now()+timedelta(seconds=int(p.get('seconds')))
